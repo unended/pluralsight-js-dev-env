bd15f43ce95c263f102d613830d321e22e94c2d8
Patrick Ashamalla <patrick.ashamalla@gmail.com>
2017-03-05 17:56:28 -0500

    Mock data and HTTP setup

---
+++ src/api/userApi.js
@@ -0,0 +1,34 @@
+import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
+
+// Public function
+export function getUsers() {
+    return get('users');
+}
+
+export function deleteUser(id) {
+    return del(`users/${id}`);
+}
+
+// Private functions below
+function get(url) {
+    return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url) {
+    const request = new Request(baseUrl + url, {
+        method: 'DELETE'
+    });
+
+    return fetch(request).then(onSuccess, onError);
+}
+
+function onSuccess(response) {
+    return response.json();
+}
+
+function onError(error) {
+    console.log(error); // eslint-disable-line no-console
+}
