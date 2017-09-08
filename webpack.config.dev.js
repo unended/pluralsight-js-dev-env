f7b652fca92c6a553d1edb763c2c3a169618e7ad
Patrick Ashamalla <patrick.ashamalla@gmail.com>
2017-09-08 18:59:58 -0400

    updated to work with webpack 3

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,9 +2,11 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-    debug: true,
+    // debug: true,
     devtool: 'inline-source-map',
-    noInfo: false,
+    devServer: {
+        noInfo: false
+    },
     entry: [
         path.resolve(__dirname, 'src/index')
     ],
@@ -22,9 +24,9 @@ export default {
         })
     ],
     module: {
-        loaders: [
-            {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-            {test: /\.css$/, loaders: ['style','css']}
+        rules: [
+            {test: /\.js$/, exclude: /node_modules/, use: ['babel-loader']},
+            {test: /\.css$/, use: ['style-loader','css-loader']}
         ]
     }
 }
